45d1bc4cfe202662a801745e9030f73794d87956
onnttnno <sanamori@msn.com>
2014-09-02 19:46:31 +0700

    5710742130_EX1.8

---
+++ 5710742130_EX1.8/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,x;
+    printf("High: ");
+    scanf ("%d",&a);
+    printf ("Wide: ");
+    scanf ("%d",&b);
+    x = (a*b)/2;
+    printf ("Sum: %d",x);
+    return 0;
+}
